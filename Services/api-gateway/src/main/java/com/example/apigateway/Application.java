aa16cb065eceb286986da102f6191613601a9868
rajadilipkolli <rajakolli@deloitte.com>
2021-03-22 10:52:10 +0530

    convert to maven project

--- Services/api-gateway/src/main/java/com/example/apigateway/Application.java
+++ Services/api-gateway/src/main/java/com/example/apigateway/Application.java
@@ -1,32 +1,29 @@
 package com.example.apigateway;
 
+import com.example.apigateway.models.ERole;
+import com.example.apigateway.models.Role;
+import com.example.apigateway.repository.RoleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-import com.example.apigateway.models.ERole;
-import com.example.apigateway.models.Role;
-import com.example.apigateway.repository.RoleRepository;
-
 @EnableFeignClients
 @SpringBootApplication
 public class Application implements CommandLineRunner {
 
-	@Autowired
-	private RoleRepository roleRepository;
+    @Autowired private RoleRepository roleRepository;
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 
-	@Override
-	public void run(String... args) throws Exception {
-		if (!roleRepository.findByName(ERole.ROLE_USER).isPresent()) {
-			Role roleUser = new Role(ERole.ROLE_USER);
-			this.roleRepository.save(roleUser);
-		}
-
-	}
+    @Override
+    public void run(String... args) throws Exception {
+        if (!roleRepository.findByName(ERole.ROLE_USER).isPresent()) {
+            Role roleUser = new Role(ERole.ROLE_USER);
+            this.roleRepository.save(roleUser);
+        }
+    }
 }
