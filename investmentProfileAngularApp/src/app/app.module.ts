9b1db37fb3e9d58a403d49ad5f3d08434fbfdff6
rajadileepkolli <rajadileepkolli@gmail.com>
2021-05-23 10:20:49 +0530

    Generated modules

--- investmentProfileAngularApp/src/app/app.module.ts
+++ investmentProfileAngularApp/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SipComponent } from './sip/sip.component';
+import { StocksComponent } from './stocks/stocks.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { AuthComponent } from './auth/auth.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SipComponent,
+    StocksComponent,
+    PortfolioComponent,
+    AuthComponent
   ],
   imports: [
     BrowserModule,
---
+++ investmentProfileAngularApp/src/app/sip/sip.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sip',
+  templateUrl: './sip.component.html',
+  styleUrls: ['./sip.component.css']
+})
+export class SipComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
