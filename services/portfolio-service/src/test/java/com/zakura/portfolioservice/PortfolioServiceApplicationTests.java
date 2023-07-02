726307304858d895e3b223a4af9700183e36d24d
Raja Kolli <rajadilipkolli@gmail.com>
2023-07-02 18:27:45 +0000

    add test containers in dev mode and push image

--- services/portfolio-service/src/test/java/com/zakura/portfolioservice/PortfolioServiceApplicationTests.java
+++ services/portfolio-service/src/test/java/com/zakura/portfolioservice/PortfolioServiceApplicationTests.java
@@ -3,8 +3,8 @@ package com.zakura.portfolioservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class PortfolioServiceApplicationTests extends AbstractMongoDBTestContainer {
+@SpringBootTest(classes = TestPortfolioServiceApplication.class)
+class PortfolioServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
---
+++ services/portfolio-service/src/test/java/com/zakura/portfolioservice/TestPortfolioServiceApplication.java
@@ -0,0 +1,38 @@
+package com.zakura.portfolioservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.MongoDBContainer;
+
+@TestConfiguration(proxyBeanMethods = false)
+public class TestPortfolioServiceApplication {
+    
+	@Bean
+	@ServiceConnection
+	MongoDBContainer mongoDbContainer() {
+		return new MongoDBContainer("mongo:latest");
+	}
+
+	@Bean
+	@ServiceConnection(name = "openzipkin/zipkin")
+	GenericContainer<?> zipkinContainer() {
+		return new GenericContainer<>("openzipkin/zipkin:latest").withExposedPorts(9411);
+	}
+
+	@Bean
+	GenericContainer<?> discoveryServiceContainer(DynamicPropertyRegistry dynamicPropertyRegistry) {
+		GenericContainer<?> discoveryServiceContainer = new GenericContainer<>("dockertmt/discovery-service")
+				.withExposedPorts(8761);
+		dynamicPropertyRegistry.add("eureka.client.service-url.defaultZone", () -> String.format("http://%s:%d/eureka",
+				discoveryServiceContainer.getHost(), discoveryServiceContainer.getMappedPort(8761)));
+		return discoveryServiceContainer;
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.from(PortfolioServiceApplication::main).with(TestPortfolioServiceApplication.class).run(args);
+	}
+}
