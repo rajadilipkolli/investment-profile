bceb2bd5a71eda9652bb37d32e85444002e30c27
Raja Kolli <rajadilipkolli@gmail.com>
2023-07-02 18:13:52 +0000

    adds discoveryServiceContainer for dev mode

--- services/sip-service/src/test/java/com/example/sipservice/TestSipServiceApplication.java
+++ services/sip-service/src/test/java/com/example/sipservice/TestSipServiceApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.DynamicPropertyRegistry;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.MongoDBContainer;
 
@@ -20,6 +21,15 @@ public class TestSipServiceApplication {
 	@ServiceConnection(name = "openzipkin/zipkin")
 	GenericContainer<?> zipkinContainer() {
 		return new GenericContainer<>("openzipkin/zipkin:latest").withExposedPorts(9411);
+	}
+
+	@Bean
+	GenericContainer<?> discoveryServiceContainer(DynamicPropertyRegistry dynamicPropertyRegistry) {
+		GenericContainer<?> discoveryServiceContainer = new GenericContainer<>("dockertmt/discovery-service")
+				.withExposedPorts(8761);
+		dynamicPropertyRegistry.add("eureka.client.service-url.defaultZone", () -> String.format("http://%s:%d/eureka",
+				discoveryServiceContainer.getHost(), discoveryServiceContainer.getMappedPort(8761)));
+		return discoveryServiceContainer;
 	}
 
 	public static void main(String[] args) {
