30592e37a3ca0ece545fa7a7e3ebd5743c2e47e2
Raja Kolli <rajadilipkolli@gmail.com>
2023-07-06 03:10:02 +0000

    adds Network

--- services/stock-service/src/test/java/com/zakura/stockservice/TestStockServiceApplication.java
+++ services/stock-service/src/test/java/com/zakura/stockservice/TestStockServiceApplication.java
@@ -7,26 +7,31 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.containers.Network;
 
 @TestConfiguration(proxyBeanMethods = false)
 public class TestStockServiceApplication {
 
+	static Network network = Network.newNetwork();
+
 	@Bean
 	@ServiceConnection
 	MongoDBContainer mongoDbContainer() {
-		return new MongoDBContainer("mongo:latest");
+		return new MongoDBContainer("mongo:latest").withNetwork(network).withNetworkAliases("mongodb");
 	}
 
 	@Bean
 	@ServiceConnection(name = "openzipkin/zipkin")
 	GenericContainer<?> zipkinContainer() {
-		return new GenericContainer<>("openzipkin/zipkin:latest").withExposedPorts(9411);
+		return new GenericContainer<>("openzipkin/zipkin:latest").withExposedPorts(9411)
+		.withNetwork(network)
+        .withNetworkAliases("zipkin");
 	}
 
 	@Bean
 	GenericContainer<?> discoveryServiceContainer(DynamicPropertyRegistry dynamicPropertyRegistry) {
 		GenericContainer<?> discoveryServiceContainer = new GenericContainer<>("dockertmt/discovery-service")
-				.withExposedPorts(8761);
+				.withExposedPorts(8761).withNetwork(network).withNetworkAliases("discovery-service");
 		dynamicPropertyRegistry.add("eureka.client.service-url.defaultZone", () -> String.format("http://%s:%d/eureka",
 				discoveryServiceContainer.getHost(), discoveryServiceContainer.getMappedPort(8761)));
 		return discoveryServiceContainer;
@@ -36,7 +41,7 @@ public class TestStockServiceApplication {
 	GenericContainer<?> portfolioServiceContainer(GenericContainer<?> discoveryServiceContainer,
 			MongoDBContainer mongoDbContainer) {
 		return new GenericContainer<>("dockertmt/portfolio-service:0.0.1")
-				.withExposedPorts(8002)
+				.withExposedPorts(8002).withNetwork(network)
 				.dependsOn(discoveryServiceContainer, mongoDbContainer)
 				.withEnv("eureka.client.service-url.defaultZone", String.format("http://%s:%d/eureka",
 						discoveryServiceContainer.getHost(), discoveryServiceContainer.getMappedPort(8761)))
